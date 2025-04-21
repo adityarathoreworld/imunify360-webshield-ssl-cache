710fb6299d1b493e78bd0a0365ffa8ce892406ea
Aditya Rathore <57768475+adityarathoreworld@users.noreply.github.com>
2025-04-22 00:49:11 +0530

    Create webshield_ssl_cache.sh
    To see cached ssl websites run /usr/sbin/im360-ssl-cache.
    Instructions
    change permission with chmod -x  webshield_ssl_cache.sh and run this script in cron. Also, check /var/log/jsupply_new_ssl.log to see if there is any error. Require jsupply_new_ssl.py script from https://cloudlinux.zendesk.com/hc/en-us/articles/360016054460-WebShield-SSL-cache-configuration-on-Imunify360-stand-alone-installation
    How to run cron?
    crontab -e
    0 * * * * /webshield_ssl_cache.sh.

---
+++ webshield_ssl_cache.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Specify more domains
+DOMAINS=$(find /home/ -mindepth 1 -maxdepth 1 -type d ! -name 'backup' !  -name                                                                                                                                                                                                                                              'vmail')
+
+BASE_PATH="/etc/letsencrypt/live"
+
+SCRIPT_PATH="/jsupply_new_ssl.py"
+LOG_PATH="/var/log/jsupply_new_ssl.log"
+
+#provided that the certificates for each domain can be found in a similar path,                                                                                                                                                                                                                                              we can enumerate those
+for domain in $DOMAINS
+do
+    domain_base=$(basename "$domain")
+    KEY_PATH="$BASE_PATH/$domain_base/privkey.pem"
+    CERT_PATH="$BASE_PATH/$domain_base/cert.pem"
+    CHAIN_PATH="$BASE_PATH/$domain_base/fullchain.pem"
+    # Remove old cache entry, if there is a chance the certificate files will no                                                                                                                                                                                                                                             t exist at some point the deletion can me marked as text
+   python3  $SCRIPT_PATH -d $domain_base -r >> $LOG_PATH 2>&1
+
+    # Add new certificate to cache
+    python3 $SCRIPT_PATH -d $domain_base -k $KEY_PATH -c $CERT_PATH -C $CHAIN_PA                                                                                                                                                                                                                                             TH -a >> $LOG_PATH 2>&1
+done
